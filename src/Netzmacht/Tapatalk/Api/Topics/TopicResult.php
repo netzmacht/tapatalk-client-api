ce83f818f6518d52096ee44681c2540b59b1a581
David Molineus <mail@netzmacht.de>
2014-05-27 08:30:29 +0200

    add php file headers

--- src/Netzmacht/Tapatalk/Api/Topics/TopicResult.php
+++ src/Netzmacht/Tapatalk/Api/Topics/TopicResult.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * @package    tapatalk-client-api
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2014 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
 namespace Netzmacht\Tapatalk\Api\Topics;
 
 use Netzmacht\Tapatalk\Result;
--- src/Netzmacht/Tapatalk/Client.php
+++ src/Netzmacht/Tapatalk/Client.php
@@ -1,5 +1,37 @@
 <?php
 
+/**
+ * @package    tapatalk-client-api
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2014 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+/**
+ * Copyright 2014 netzmacht creative David Molineus
+ *
+ * Licensed under the GNU Lesser General Public_License (LGPL), Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gnu.org/licenses/lgpl-3.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package    tapatalk-client-api
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
 
 namespace Netzmacht\Tapatalk;
 
